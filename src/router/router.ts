d2a3d2f7163ae8a6a0087f4f2838393b97bfa1f3
Letlhogonolo <164743557+whoIsImai@users.noreply.github.com>
2026-01-05 17:37:30 +0200

    Add to routes

--- src/router/router.ts
+++ src/router/router.ts
@@ -1,10 +1,14 @@
 import { Router } from "express"
 
 import {Pay, Notify} from '../controller/payfast'
+import {CreateOrder, UpdateOrderStatus, trackingInfo} from '../controller/order'
 
 const router = Router()
 
 router.post("/pay", Pay)
 router.post("/notify_url", Notify)
+router.post("/orders/create", CreateOrder)
+router.put("/orders/:orderId/status", UpdateOrderStatus)
+router.get("/track/:orderId", trackingInfo)
 
 export default router
